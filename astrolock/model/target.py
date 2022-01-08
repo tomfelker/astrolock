8966e8d1a08f36579105ff6d7e35df44ba3b2893
tomfelker <tomfelker@gmail.com>
2022-01-07 21:50:37 -0800

    fixes, implemented acceleration tracking

--- astrolock/model/target.py
+++ astrolock/model/target.py
@@ -10,45 +10,58 @@ class Target:
         self.altaz_from_tracker = None
         self.last_known_location = None
         self.last_known_location_time = None
+        self.extrapolated_velocity_itrs = None
+        self.extrapolated_acceleration_itrs = None
         self.prev_target = None
         self.score = -float('inf')
         self.display_columns = {}
 
     # callers will do  target = target.updated_with(new_target)
     def updated_with(self, new_target):
-        self.prev_target = None
-        new_target.prev_target = self
+        old_target = self
+        old_target.prev_target = None
+        new_target.prev_target = old_target
+        
+        
+
+        time_between_updates = new_target.last_known_location_time - old_target.last_known_location_time
+        if time_between_updates > 0 * u.s:
+            delta_location_itrs = new_target.last_known_location_itrs.cartesian - old_target.last_known_location_itrs.cartesian
+            new_target.extrapolated_velocity_itrs = delta_location_itrs / time_between_updates
+            if old_target.extrapolated_velocity_itrs is not None:
+                delta_velocity_itrs = new_target.extrapolated_velocity_itrs - old_target.extrapolated_velocity_itrs
+                new_target.extrapolated_acceleration_itrs = delta_velocity_itrs / time_between_updates
+
         return new_target
 
-    def altaz_and_rates_at_time(self, tracker, time, hack = True):
-        if self.prev_target is not None:
-            time_between_updates = self.last_known_location_time - self.prev_target.last_known_location_time
-            if time_between_updates > 0 * u.s:
-                time_since_penultimate_update = time - self.last_known_location_time
+    def altaz_and_rates_at_time(self, tracker, time, dt = 1.0 * u.s, hack = True):
+        time_since_update = time - self.last_known_location_time
+        location_itrs_at_time = self.last_known_location_itrs.cartesian
+        
+        if self.extrapolated_velocity_itrs is not None:
+            location_itrs_at_time += self.extrapolated_velocity_itrs * time_since_update
 
-                prev_to_current_location_itrs = self.last_known_location_itrs.cartesian - self.prev_target.last_known_location_itrs.cartesian
-                velocity_itrs = prev_to_current_location_itrs / time_between_updates
-                location_itrs_at_time = astropy.coordinates.ITRS(self.prev_target.last_known_location_itrs.cartesian + velocity_itrs * time_since_penultimate_update)
+        if self.extrapolated_acceleration_itrs is not None:
+            location_itrs_at_time += 0.5 * self.extrapolated_acceleration_itrs * time_since_update * time_since_update
 
-                # todo: fix this...
-                # really shouldn't need to specify a time here, but astropy will crash if we don't - presumably it's trying to transform through a solar system barycentric frame
-                #todo: cache this
-                tracker_altaz = astropy.coordinates.AltAz(location = tracker.location_ap, obstime = 'J2000')
+        location_itrs_at_time = astropy.coordinates.ITRS(location_itrs_at_time)
 
-                altaz = astrolock.model.astropy_util.itrs_to_altaz_direct(location_itrs_at_time, tracker_altaz)
-                
-                rates = np.zeros(2) * u.deg / u.s
-                if hack:
-                    dt = 1.0 * u.s
-                    future_altaz, dummy_rates = self.altaz_and_rates_at_time(tracker = tracker, time = time + dt, hack = False)
-                    rates = [
-                        (future_altaz.az - altaz.az) / dt,
-                        (future_altaz.alt - altaz.alt) / dt
-                    ]
-                    
-                return altaz, rates
-        return self.altaz_from_tracker, np.zeros(2) * u.deg / u.s
+        # todo: fix this...
+        # really shouldn't need to specify a time here, but astropy will crash if we don't - presumably it's trying to transform through a solar system barycentric frame
+        #todo: cache this
+        tracker_altaz = astropy.coordinates.AltAz(location = tracker.location_ap, obstime = 'J2000')
 
+        altaz = astrolock.model.astropy_util.itrs_to_altaz_direct(location_itrs_at_time, tracker_altaz)
+        
+        rates = np.zeros(2) * u.deg / u.s
+        if hack:
+            future_altaz, dummy_rates = self.altaz_and_rates_at_time(tracker = tracker, time = time + dt, hack = False)
+            rates = [
+                (future_altaz.az - altaz.az).wrap_at(180 * u.deg) / dt,
+                (future_altaz.alt - altaz.alt).wrap_at(180 * u.deg) / dt
+            ]
+            
+        return altaz, rates
                 
 
                 
